abfecc5d52d224a40dad573156f2fe514e7c5e82
Daniel Falabella <dnlfala@gmail.com>
2016-09-15 07:20:54 -0600

    Only displaying private lists and posts to the public.

--- controllers/Pages.js
+++ controllers/Pages.js
@@ -6,7 +6,7 @@ var Routes   = module.exports = {},
 Routes.index = function (req, res) {
   List.find({owner: req.user && req.user._id || null})
   .then(function(lists) {
-    Post.find({})
+    Post.find({isPrivate: false})
     .sort('-created_date')
     .populate({ path: 'owner', select: 'displayName _id lists image' })
     .exec(function (err, result) {
@@ -26,11 +26,23 @@ Routes.userView = function (req, res) {
   if (!userId && req.user && req.user._id) return res.redirect('/user/' + req.user._id);
   if (!userId) return res.redirect('/');
 
+  var userIsListOwner = false;
+  if (req.user && req.user._id && (req.params.userId == req.user._id))
+    userIsListOwner = true;
+
   List.find({owner: userId})
   .populate({path: 'posts', options: { sort: { 'created_date': -1 } }})
   .exec(function(error, result) {
     User.findById(userId, function (err, user) {
       if (err) return res.status(400).json(err);
+
+      if (!userIsListOwner) {
+        result = result.filter(function (list) {
+          if (list.isPrivate) return false;
+          return true;
+        })
+      };
+
       res.render('user', {
         user: req.user || null,
         userId: userId,
