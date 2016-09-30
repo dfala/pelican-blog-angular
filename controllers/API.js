cbcde20512f05b255771733116a758163369d70a
Daniel Falabella <dnlfala@gmail.com>
2016-09-30 11:39:13 -0600

    Built get post api for app.

--- controllers/API.js
+++ controllers/API.js
@@ -11,6 +11,7 @@ module.exports = function (app) {
   app.put('/api/list/rename/:listId', List.renameList);
 
   //POST
+  app.get('/api/post/:postId', Post.get);
   app.post('/api/post', Post.create);
   app.put('/api/post/:postId', Post.update);
   app.delete('/api/post/:listId/:postId', Post.delete);
