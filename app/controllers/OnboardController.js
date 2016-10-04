93975ea8981e98ae7d3dbf9e8d1ba4c95c506b0a
Daniel Falabella <dnlfala@gmail.com>
2016-10-04 06:31:43 -0600

    Adding functionality to on boarding modals.

--- app/controllers/OnboardController.js
+++ app/controllers/OnboardController.js
@@ -2,6 +2,21 @@ angular.module('Pelican')
 
 .controller('OnboardController', ['$scope', '$rootScope', 'apiService', function ($scope, $rootScope, apiService) {
 
+  $scope.init = function () {
+    $scope.active = 1;
+  };
+
+  $scope.next = function () {
+    $scope.active = $scope.active + 1;
+  };
+
+  $scope.previous = function () {
+    $scope.active = $scope.active - 1;
+  };
+
+  $scope.goTo = function (numb) {
+    $scope.active = numb;
+  };
 
 
 }]);
