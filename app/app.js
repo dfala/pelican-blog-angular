def9e90b6a521d1c64ba6fc5196bcf22716632b6
Daniel Falabella <dnlfala@gmail.com>
2016-11-09 19:56:02 -0700

    First commit one page app.

--- app/app.js
+++ app/app.js
@@ -1,1 +1,26 @@
-angular.module('Pelican', ['ngSanitize'])
+var routerApp = angular.module('Pelican', ['ngSanitize','ui.router']);
+
+routerApp.config(function($stateProvider, $urlRouterProvider) {
+
+    $urlRouterProvider.otherwise('/home');
+
+    $stateProvider
+
+        // HOME STATES AND NESTED VIEWS ========================================
+        .state('user', {
+            url: '/user',
+            templateUrl: './templates/user.html',
+            controller: 'ListsController',
+            resolve: {
+              ownerLists: function (apiService) {
+                // return apiService.get
+              }
+            }
+        })
+
+        // ABOUT PAGE AND MULTIPLE NAMED VIEWS =================================
+        .state('about', {
+            // we'll get to this in a bit
+        });
+
+});
